f071829264ae4b8184ade30a1ae5e1748f90c3e2
zhangxg <zhangxg2@lenovo.com>
2015-06-09 18:08:19 +0800

    do some basic testing

--- djangoweb/cloudmonitor/integration/api/connector.py
+++ djangoweb/cloudmonitor/integration/api/connector.py
@@ -1,4 +1,8 @@
+from __future__ import with_statement
+
 __author__ = 'zhangxg'
+
+# from __future__ import with_statement
 
 '''
 this module connects the openstack api
@@ -6,6 +10,8 @@ this module connects the openstack api
 
 from httplib import HTTPConnection
 from urlparse import urlparse
+
+
 
 
 # from .. import config  fixme: the import not working
@@ -37,3 +43,76 @@ def get_connection(url):
 #             return HTTPConnection(parsed_url.hostname, parsed_url.port, {'timeout': config['timeout']})
 #         else:
 #             return self.connection
+
+# class MyException(Exception):
+#
+#     def __init__(self):
+#         self.code = 1
+#         self.message = 'this is my exception'
+#         self.__str__ = 'my exception returns a string'
+#
+#         # print('user defined')
+#
+#
+# b = 10
+# try:
+#     try:
+#         a = b / 0
+#     except ZeroDivisionError as zero_exp:
+#         #
+#         print(zero_exp)
+#     else:
+#         print('this is internal else')
+#
+#     finally:
+#         print('internal finally')
+#
+#     print zero_exp.message
+#
+# except MyException as myexp:
+#     print('catch my exception' + myexp.message + str(myexp.code))
+#
+# else:
+#     print('this is outer else')
+#
+# finally:
+#     print('finally')
+#
+# # print(myexp)
+# print('end of programming')
+
+
+#
+# with open('path.to.file', 'r') as file:
+#     print(file.read())
+
+
+class TraceBlock:
+    def message(self, arg):
+        print('running ' + arg)
+
+    def __enter__(self):
+        print('entering...')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+
+        if exc_type is None:
+            print('exist normally...')
+        else:
+            print('raise excepiton... ' + str(exc_type))
+            return False
+
+if __name__ == '__main__':
+    # with TraceBlock() as action:
+    #     action.message('test 1')
+    #     print('reached')
+    with TraceBlock() as action:
+        action.message('test 2')
+        raise TypeError
+        print('not reached')
+
+
+
+
+
